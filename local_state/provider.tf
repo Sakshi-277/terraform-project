b90f3ad9ab746c79395d30918ab94e004df61002
Sakshi-277 <134872767+Sakshi-277@users.noreply.github.com>
2024-09-22 23:22:05 +0530

    Update provider.tf

--- local_state/provider.tf
+++ local_state/provider.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.3.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  subscription_id = "aeee6685-fdd2-4723-942b-30faf6b496b7"
+  tenant_id = "f36ff4fc-aaa7-4451-b65b-f1f10c9e7918"
+  features{}
+}
